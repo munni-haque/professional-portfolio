337189336c876cf77c73e72e712bc26fb0f1f02e
munni-haque <munnih695@gmail.com>
2024-02-25 11:17:57 +0600

    commit message

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('navbar');
+let sections = document.querySelector('section');
+let navlinks = document.querySelector('header nav a');
+
+
+window.onscroll = ()
